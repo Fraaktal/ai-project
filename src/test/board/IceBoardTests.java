bb6e0db7c04f46cd11eb50271568f6eea2a6e40d
Sophie Nguyen <chaussettefurtive@protonmail.com>
2022-05-13 17:07:45 +0200

    [CHG] Reorganize tests

--- src/test/board/IceBoardTests.java
+++ src/test/board/IceBoardTests.java
@@ -16,10 +16,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 class IceboardTests {
-    private final Iceboard iceboard = new Iceboard();
 
     @Test
     void possibleMovesTest() {
+        Iceboard iceboard = new Iceboard();
+
         Set<String> possibleMoves = iceboard.getPossibleMoves(IcebergRole.RED);
         assertEquals(9, possibleMoves.size());
         Set<String> expectedMoves = new HashSet<>(Arrays.asList("A1-A2", "A1-B1", "A1-B2", "E9-D8", "E9-E8", "E9-F8", "I1-H1", "I1-H2", "I1-I2"));
@@ -29,37 +30,39 @@ class IceboardTests {
         assertEquals(9, possibleMoves.size());
         expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
         assertTrue(expectedMoves.containsAll(possibleMoves));
+    }
 
-        /*
-        Iceboard iceboard1 = new Iceboard();
-        iceboard1.load(getClass().getResource("/resource/plateau1.txt").getPath());
-        Set<String> possibleMoves = iceboard1.getPossibleMoves(IcebergRole.RED);
-        System.out.println(possibleMoves);
+    @Test
+    void getPossibleMoves1Test() {
+        Iceboard iceboard = new Iceboard();
+        iceboard.load(getClass().getResource("/resource/plateau1.txt").getPath());
+
+        Set<String> possibleMoves = iceboard.getPossibleMoves(IcebergRole.RED);
+        Set<String> expectedMoves = new HashSet<>(Arrays.asList("A1-A2", "A1-B2", "A1-B1", "E9-E8", "E9-D8", "I1-I2"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));
+
+        possibleMoves = iceboard.getPossibleMoves(IcebergRole.BLACK);
+        expectedMoves = new HashSet<>(Arrays.asList("C6-C5", "I5-I4", "E1-E2"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));
+    }
+
+    @Test
+    void getPossibleMoves2Test() {
+        Iceboard iceboard = new Iceboard();
+        iceboard.load(getClass().getResource("/resource/plateau2.txt").getPath());
+
+        Set<String> possibleMoves = iceboard.getPossibleMoves(IcebergRole.RED);
         assertEquals(11, possibleMoves.size());
-        // TODO:
         Set<String> expectedMoves = new HashSet<>(Arrays.asList("C4-C3", "C4-D4", "C4-D5", "H2-H1", "H2-H3", "H2-G2", "H2-G3", "H2-I2", "E9-E8", "E9-D8", "E9-F8"));
         assertTrue(expectedMoves.containsAll(possibleMoves));
-        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.BLACK);
-        // TODO:
-        expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
-        assertTrue(expectedMoves.containsAll(possibleMoves));*/
-
-        Iceboard iceboard1 = new Iceboard();
-        iceboard1.load(getClass().getResource("/resource/plateau2.txt").getPath());
-        System.out.println(iceboard1);
-        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.RED);
-        System.out.println(possibleMoves);
-        assertEquals(11, possibleMoves.size());
-        expectedMoves = new HashSet<>(Arrays.asList("C4-C3", "C4-D4", "C4-D5", "H2-H1", "H2-H3", "H2-G2", "H2-G3", "H2-I2", "E9-E8", "E9-D8", "E9-F8"));
-        assertTrue(expectedMoves.containsAll(possibleMoves));
-        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.BLACK);
-        // TODO:
-        expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
+        possibleMoves = iceboard.getPossibleMoves(IcebergRole.BLACK);
+        expectedMoves = new HashSet<>(Arrays.asList("E1-E2", "E1-D1", "E1-F1", "H5-H4", "H5-H6", "H5-G5", "H5-G6", "H5-I4", "E6-E5", "E6-E7", "E6-D5", "E6-F5", "E6-F6"));
         assertTrue(expectedMoves.containsAll(possibleMoves));
     }
 
     @Test
     void moveTest() {
+        Iceboard iceboard = new Iceboard();
         Set<String> possibleMoves = iceboard.getPossibleMoves(IcebergRole.RED);
         String move = possibleMoves.iterator().next();
         IceMove iceMove = new IceMove(move);
