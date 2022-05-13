2b6e9764f35aed8b5da185a64eba39c5ffe75b4e
Sophie Nguyen <chaussettefurtive@protonmail.com>
2022-05-13 16:15:24 +0200

    [NEW] Add tests for other boards (not finished)

--- src/test/board/IceBoardTests.java
+++ src/test/board/IceBoardTests.java
@@ -29,6 +29,33 @@ class IceboardTests {
         assertEquals(9, possibleMoves.size());
         expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
         assertTrue(expectedMoves.containsAll(possibleMoves));
+
+        /*
+        Iceboard iceboard1 = new Iceboard();
+        iceboard1.load(getClass().getResource("/resource/plateau1.txt").getPath());
+        Set<String> possibleMoves = iceboard1.getPossibleMoves(IcebergRole.RED);
+        System.out.println(possibleMoves);
+        assertEquals(11, possibleMoves.size());
+        // TODO:
+        Set<String> expectedMoves = new HashSet<>(Arrays.asList("C4-C3", "C4-D4", "C4-D5", "H2-H1", "H2-H3", "H2-G2", "H2-G3", "H2-I2", "E9-E8", "E9-D8", "E9-F8"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));
+        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.BLACK);
+        // TODO:
+        expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));*/
+
+        Iceboard iceboard1 = new Iceboard();
+        iceboard1.load(getClass().getResource("/resource/plateau2.txt").getPath());
+        System.out.println(iceboard1);
+        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.RED);
+        System.out.println(possibleMoves);
+        assertEquals(11, possibleMoves.size());
+        expectedMoves = new HashSet<>(Arrays.asList("C4-C3", "C4-D4", "C4-D5", "H2-H1", "H2-H3", "H2-G2", "H2-G3", "H2-I2", "E9-E8", "E9-D8", "E9-F8"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));
+        possibleMoves = iceboard1.getPossibleMoves(IcebergRole.BLACK);
+        // TODO:
+        expectedMoves = new HashSet<>(Arrays.asList("A5-A4", "A5-B5", "A5-B6", "E1-E2", "E1-D1", "E1-F1", "I5-I4", "I5-H5", "I5-H6"));
+        assertTrue(expectedMoves.containsAll(possibleMoves));
     }
 
     @Test
